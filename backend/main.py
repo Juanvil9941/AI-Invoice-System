d16a6b388369a630eab37ff1bfa0ca1d305e5dc8
saiteja6006 <saiteja6006@gmail.com>
2026-04-08 16:23:05 -0500

    Fix CORS issue

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ def root():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://ai-invoice-system.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
